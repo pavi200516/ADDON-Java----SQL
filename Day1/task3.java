1a890c3cabe6a752a38cb5e9807f3336666d07b4
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-11 09:53:45 +0530

    Create task3.java

---
+++ Day1/task3.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		int p=1000;
+		int r=5;
+		int t=2;
+		int mul=p*r*t;
+		int div=mul/100;
+		System.out.print("Interest is:");
+		System.out.print(div);
+	}
+
+}
