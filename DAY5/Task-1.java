929bd5ff831539122bf860506d7653b92a7e73f4
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-11 12:22:11 +0530

    Create Task-1.java

---
+++ DAY5/Task-1.java
@@ -0,0 +1,18 @@
+import java .util.Scanner;
+//DIVIDE BY ZERO
+public class Exception {
+	
+      public static void main(String[] args) {
+    	  Scanner in = new Scanner(System.in);
+    			  try{
+    				  System.out.println("Enter the number:");
+    				  int number = in.nextInt();
+    				  int total = 10 / number;
+    				  System.out.println("Total:"+total);
+    			  }
+    			  catch(Exception e) {     
+    				  System.out.println(e);
+    				  }
+    			  }
+
+}
