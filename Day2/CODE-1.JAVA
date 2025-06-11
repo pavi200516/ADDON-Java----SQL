e3f3232a6ebdf834665e6f2964c9912c6d371b61
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-11 09:56:41 +0530

    Create CODE-1.JAVA

---
+++ Day2/CODE-1.JAVA
@@ -0,0 +1,14 @@
+package program;
+
+public class demo1 {
+	
+	
+	public static void Details(String name,String job,String Package) {
+		System.out.println("My Name is "+name+".My job is "+job+".My package is "+Package);
+	}
+	public static void main(String[] args) {
+		Details("Jumana","Data analyst","4LPA");
+		Details("Noufiya","Data scientist","4LPA");
+	}
+
+}
