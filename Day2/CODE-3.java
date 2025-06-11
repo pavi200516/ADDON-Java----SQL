d25d94b94d2196816416dd17ab89d52dd76d7be2
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-11 09:59:07 +0530

    Create CODE-3.java

---
+++ Day2/CODE-3.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String a=s.next();
+        System.out.println("Enter your Contact number:");
+        int b=s.nextInt();
+        System.out.println("Enter your Email id:");
+        String c=s.next();
+        System.out.println("Enter your Address:");
+        String d=s.next();
+        System.out.println("Enter your Skillset:");
+        String e=s.next();
+        System.out.println("Enter your Project Description:");
+        String f=s.next();
+        System.out.println("My name is:"+a);
+        System.out.println("My Contact number:"+b);
+        System.out.println("My Email ID:"+c);
+        System.out.println("My Address:"+d);
+        System.out.println("My Skillset:"+e);
+        System.out.println("My Project Description:"+f);
+    }
+}
+        
