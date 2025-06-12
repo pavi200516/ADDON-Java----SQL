68e9b244d6ff1531aa86cf5dd1ffb8c73ad579cc
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-12 10:40:49 +0530

    Create Task-1.java

---
+++ DAY6/Task-1.java
@@ -0,0 +1,23 @@
+package Day6;
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Task1 {
+	public static void main(String[] args) {
+		ArrayList<Object> arr=new ArrayList<Object>();
+		arr.add("ABC");
+		arr.add("B");
+		arr.add("Computer Technology");
+		arr.add("Final Year");
+		arr.add("DEF");
+		arr.add(2);
+		arr.add(5,"kg");
+		arr.add(6,"Web Develop");
+		for(Object data:arr) {                                                                                                                     m                                                                    
+		    System.out.println(data);
+		    
+       }
+	
+    }
+
+}
